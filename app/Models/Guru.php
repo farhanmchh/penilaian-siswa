815e8aa2bcfbfe2e85982551d219d66eaa2b0e60
farhanmchh <moch.farhan04@gmail.com>
2022-01-22 00:40:01 +0700

    [Add] Login & Logout for All Role

---
+++ app/Models/Guru.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guru extends Model
+{
+  use HasFactory;
+
+  protected $table = 'guru';
+  protected $fillable = ['nip', 'nama_guru', 'jk', 'alamat', 'password'];
+  protected $primaryKey = 'nip';
+  public $timestamps = false;
+}
---
+++ app/Models/Siswa.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+  use HasFactory;
+
+  protected $table = 'siswa';
+  protected $fillable = ['nis', 'nama_siswa', 'jk', 'alamat', 'id_kelas', 'password'];
+  protected $primaryKey = 'nis';
+  public $timestamps = false;
+}
